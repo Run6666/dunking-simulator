1448ecb0882e94d1939a99bc0a6c46af72222309
Run <85713658+Run6666@users.noreply.github.com>
2022-02-27 19:13:35 -0500

    Update output-ATzo.lua

--- output-ATzo.lua
+++ output-ATzo.lua
@@ -1,1 +1,6 @@
-loadstring(game:HttpGetAsync("https://scripts.runison.top/dunkingsimulator.lua", true))()
+-- You can now walk around with autofarm
+-- Might not work on free exploits
+-- Doesn't work? Dm Me on Discord Run#6666
+
+-- autobuy is in beta, can be buggy
+loadstring(game:HttpGetAsync("https://rawscripts.net/raw/Dunking-Simulator-(for-raw)_939", true))()
