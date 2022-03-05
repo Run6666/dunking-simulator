a1ce4ea0fe5b3af9bd0825197dcae68138d7b55a
Run <85713658+Run6666@users.noreply.github.com>
2022-03-05 10:15:03 -0500

    Update dunkingsimulator.lua

--- dunkingsimulator.lua
+++ dunkingsimulator.lua
@@ -1,1 +1,6 @@
-loadstring(game:HttpGetAsync("https://scripts.runison.top/dunkingsimulator.lua", true))()
+-- You can now walk around with autofarm
+-- Might not work on free exploits
+-- Doesn't work? Dm Me on Discord Run#6666
+
+-- autobuy is fixed, should work fine now (maybe, maybe not)
+loadstring(game:HttpGetAsync("https://rawscripts.net/raw/Dunking-Simulator-(for-raw)_939", true))()
